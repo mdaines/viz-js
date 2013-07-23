b7f55074ba8903aa2a75ec340b897c00f686c8cc
Kyle P Davis <kdavis@riverainc.com>
2013-07-22 22:42:33 -0400

    reverted removal of cleanup code
    Put in a call to gvFinalize() to mimic the execution of the dot command-line tool

--- viz.c
+++ viz.c
@@ -15,13 +15,14 @@ __attribute__((used)) char* vizRenderFromString(char *string, char *format) {
   Agraph_t *graph = agmemread(string);
   gvLayout(context, graph, "dot");	//NOTE: customizable via the 'layout' graph attribute
 
-  //gvFreeLayout(context, graph);
-  //agclose(graph);
-  //gvFreeContext(context);
   char *result;
   unsigned int length;
   gvRenderData(context, graph, format, &result, &length);
 
+  gvFreeLayout(context, graph);
+  agclose(graph);
+  gvFinalize(context);
+  gvFreeContext(context);
 
   return result;
 }

0ce80a2b08083067267a4a1458a926f0ca934f40
Kyle P Davis <kdavis@riverainc.com>
2013-07-22 21:48:51 -0400

    camelCased viz (not a class); add fn + pkg exports

--- post.js
+++ post.js
@@ -1,3 +1,11 @@
   Module["ccall"]("vizRenderFromString", "number", ["string", "string"], [src, format]);
   return Module["return"];
+};
+
+// export both as package namespace and as function directly
+if(typeof module !== "undefined" && module !== null && module["exports"] !== null){
+	module["exports"] = viz["viz"] = viz["Viz"] = viz;
+}else{
+	this["viz"] = this["Viz"] = viz;
 }
+

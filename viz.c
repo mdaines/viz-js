6d340f253bd07c9fdb59c97f2326b33a7b82dc21
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-03-13 18:55:38 +0400

    Fixed memory leak and cached Graphviz context
    There is no need to create and destroy the context every time.
    The returned pointer to char used to leak; now we save the last
    result and free it before returning the next one.

--- viz.c
+++ viz.c
@@ -5,15 +5,19 @@ extern gvplugin_library_t gvplugin_dot_layout_LTX_library;
 extern gvplugin_library_t gvplugin_neato_layout_LTX_library;
 extern gvplugin_library_t gvplugin_core_LTX_library;
 
+GVC_t *context = NULL;
+char *last_result = NULL;
+
 __attribute__((used)) char* vizRenderFromString(const char *string,
                                                 const char *format,
                                                 const char *engine) {
-  
-  GVC_t *context = gvContext();
-  gvAddLibrary(context, &gvplugin_core_LTX_library);
-  gvAddLibrary(context, &gvplugin_dot_layout_LTX_library);
-  gvAddLibrary(context, &gvplugin_neato_layout_LTX_library);
-  
+  if (context == NULL) {
+    context = gvContext();
+    gvAddLibrary(context, &gvplugin_core_LTX_library);
+    gvAddLibrary(context, &gvplugin_dot_layout_LTX_library);
+    gvAddLibrary(context, &gvplugin_neato_layout_LTX_library);
+  }
+
   Agraph_t *graph = agmemread((char*)string);
   gvLayout(context, graph, engine);
 
@@ -23,8 +27,11 @@ __attribute__((used)) char* vizRenderFromString(const char *string,
 
   gvFreeLayout(context, graph);
   agclose(graph);
-  gvFinalize(context);
-  gvFreeContext(context);
+
+  if (last_result != NULL) {
+    gvFreeRenderData(last_result);
+  }
+  last_result = result;
 
   return result;
 }

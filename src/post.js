8cac58e5a7e8c81a8b90ccfa060c6eebd6b46e4e
Michael Daines <mdaines@fastmail.com>
2015-09-01 16:48:34 -0400

    Don't initialize the module until Viz is actually called.

--- src/post.js
+++ src/post.js
@@ -1,8 +1,11 @@
-  var module = Module();
+  var module;
   return function(src, options) {
     options = options || {};
     var format = options.format || "svg";
     var engine = options.engine || "dot";
+    if (typeof module === "undefined") {
+      module = Module();
+    }
     return module["ccall"]("vizRenderFromString", "string", ["string", "string", "string"], [src, format, engine]);
   }
 })();

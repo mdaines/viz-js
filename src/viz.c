efa37d658585f45f59e4357c1838ed9252d453e8
Michael Daines <mdaines@fastmail.com>
2015-09-22 07:27:38 -0400

    Remove unnecessary lt_preloaded_symbols

--- src/viz.c
+++ src/viz.c
@@ -4,13 +4,6 @@ extern gvplugin_library_t gvplugin_dot_layout_LTX_library;
 extern gvplugin_library_t gvplugin_neato_layout_LTX_library;
 extern gvplugin_library_t gvplugin_core_LTX_library;
 
-lt_symlist_t lt_preloaded_symbols[] = {
-	{ "gvplugin_dot_layout_LTX_library", (void *) (&gvplugin_dot_layout_LTX_library) },
-	{ "gvplugin_neato_layout_LTX_library", (void *)(&gvplugin_neato_layout_LTX_library) },
-	{ "gvplugin_core_LTX_library", (void *) (&gvplugin_core_LTX_library) },
-	{ 0, 0 }
-};
-
 GVC_t *context = NULL;
 
 __attribute__((used)) char* vizRenderFromString(const char *string, const char *format, const char *engine) {

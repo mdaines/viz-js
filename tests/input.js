17eb78d19aa6b0e0db9db45bb49a9d690c4b2c63
caseywatts <casey.s.watts@gmail.com>
2015-10-30 13:01:37 -0400

    add test to confirm Viz.js can recover from an error

--- tests/input.js
+++ tests/input.js
@@ -1,5 +1,22 @@
 QUnit.module("input");
 
+QUnit.test("recovers from an error", function(assert) {
+  var successRender = function() {
+    return Viz('digraph G { A -> "B" }', { format: "xdot" });
+  };
+  var failureRender = function() {
+    return Viz('digraph G { A -> "B }', { format: "xdot" });
+  };
+
+  var attempt1 = successRender();
+  assert.ok(attempt1.match(/digraph G/), "1: valid data succeeds");
+
+  assert.throws(failureRender, /error/, "2: invalid data fails");
+
+  var attempt3 = successRender();
+  assert.ok(attempt3.match(/digraph G/), "3: valid data succeeds again");
+});
+
 QUnit.test("result from first graph in input is returned for multiple invocations", function(assert) {
   var result = Viz("digraph A {} digraph B {}", { format: "xdot" });
   assert.ok(result.match(/digraph A/), "Result should contain \"digraph A\"");

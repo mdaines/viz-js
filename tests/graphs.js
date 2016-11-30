c493479d0035da9d0e042eae0a7d6454416710a5
Michael Daines <mdaines@fastmail.com>
2016-11-30 09:32:13 -0500

    Add sanity checks for some basic graphs to tests.

---
+++ tests/graphs.js
@@ -0,0 +1,21 @@
+QUnit.module("graphs");
+
+QUnit.test("rendering sample graphs should not throw errors", function(assert) {
+  
+  var graphs = ["./graphs/shapes.dot", "./graphs/subgraphs.dot"];
+  
+  assert.expect(graphs.length);
+  
+  graphs.forEach(function(url) {
+    var done = assert.async();
+    
+    var request = new XMLHttpRequest();
+    request.addEventListener("load", function() {
+      assert.ok(Viz(this.responseText));
+      done();
+    });
+    request.open("GET", url);
+    request.send();
+  });
+  
+});

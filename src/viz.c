2257a4017ed3bdc4bbf635279d9cd066091a425b
Michael Daines <mdaines@fastmail.com>
2017-01-13 12:05:36 -0500

    Create a new instance of Module for every call of Viz. Access errors via a new vizLastErrorMessage C API.

--- src/viz.c
+++ src/viz.c
@@ -6,16 +6,20 @@ extern gvplugin_library_t gvplugin_neato_layout_LTX_library;
 extern gvplugin_library_t gvplugin_core_LTX_library;
 
 GVC_t *context = NULL;
+char *errorMessage = NULL;
 
 int vizErrorf(char *buf) {
-  EM_ASM_({ appendError($0); }, buf);
+  errorMessage = buf;
   return 0;
 }
 
-char* vizRenderFromString(const char *src, const char *format, const char *engine) {
+char* vizLastErrorMessage() {
+  return errorMessage;
+}
 
-  Agraph_t *graph;
-  char *result;
+char* vizRenderFromString(const char *src, const char *format, const char *engine) {
+  Agraph_t *graph = NULL;
+  char *result = NULL;
   unsigned int length;
   
   if (context == NULL) {
@@ -28,25 +32,14 @@ char* vizRenderFromString(const char *src, const char *format, const char *engin
   agseterr(AGERR);
   agseterrf(vizErrorf);
   
-  // Reset line numbers.
-  agreadline(1);
-  
   while ((graph = agmemread(src))) {
     if (result == NULL) {
       gvLayout(context, graph, engine);
-      
-      // result is freed in post.js.
       gvRenderData(context, graph, format, &result, &length);
-      
-      gvFreeLayout(context, graph);
     }
     
-    agclose(graph);
-    
-    // agmemread will continue to process graphs from the initial input. Set its *new* input to the empty string so that we don't go into an infinite loop.
     src = "";
   }
-
-  return result;
   
+  return result;
 }

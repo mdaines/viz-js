95d74752396b2daab948bc6d611639770a5cdd37
Michael Daines <mdaines@fastmail.com>
2017-01-25 15:06:31 -0500

    Remove the worker memory test.

--- tests/memory.js
+++ tests/memory.js
@@ -28,33 +28,3 @@ QUnit.test("repeated invocations using setTimeout should not throw an error", fu
   
   f();
 });
-
-QUnit.test("repeated invocations in a worker should not throw an error", function(assert) {
-  var done = assert.async();
-  
-  var expected = 1000;
-  var actual = 0;
-  
-  var worker = new Worker(workerPath());
-  
-  worker.onmessage = function(e) {
-    actual += 1;
-
-    if (actual % 100 == 0) {
-      console.log(actual);
-    }
-    
-    if (actual == expected) {
-      assert.ok(true);
-      done();
-    }
-  }
-  
-  worker.onerror = function(e) {
-    throw e;
-  }
-  
-  for (var i = 0; i < expected; i++) {
-    worker.postMessage({ src: MEMORY_TEST_SRC });
-  }
-});

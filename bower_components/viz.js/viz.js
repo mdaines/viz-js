1f461617a650d277a9e0f6c7c613e410d2be6ec0
Michael Daines <mdaines@fastmail.com>
2017-03-03 16:54:29 -0500

    Update to latest Viz.js. Change "viz.js" dependency to always use the latest version.

--- bower_components/viz.js/viz.js
+++ bower_components/viz.js/viz.js
@@ -1215,6 +1215,13 @@ function v(a){void 0!==a?(e.print(a),e.X(a),a=JSON.stringify(a)):a="";w=!0;var b
     return resultString;
   }
   
+  // https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
+  function b64EncodeUnicode(str) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+      return String.fromCharCode('0x' + p1);
+    }));
+  }
+  
   Viz.svgXmlToPngImageElement = function(svgXml, scale, callback) {
     if (scale === undefined) {
       if ("devicePixelRatio" in window && window.devicePixelRatio > 1) {
@@ -1225,9 +1232,9 @@ function v(a){void 0!==a?(e.print(a),e.X(a),a=JSON.stringify(a)):a="";w=!0;var b
     }
     
     var pngImage = new Image();
-    
-    if (typeof fabric === "object" && fabric.loadSVGFromString) {
-      try {
+
+    try {
+      if (typeof fabric === "object" && fabric.loadSVGFromString) {
         fabric.loadSVGFromString(svgXml, function(objects, options) {
           // If there's something wrong with the SVG, Fabric may return an empty array of objects. Graphviz appears to give us at least one <g> element back even given an empty graph, so we will assume an error in this case.
           if (objects.length == 0) {
@@ -1255,40 +1262,50 @@ function v(a){void 0!==a?(e.print(a),e.X(a),a=JSON.stringify(a)):a="";w=!0;var b
             callback(null, pngImage);
           }
         });
-      } catch (e) {
-        if (callback !== undefined) {
-          callback(e);
-        } else {
-          throw e;
-        }
-      }
-    } else {
-      var svgImage = new Image();
+      } else {
+        var svgImage = new Image();
 
-      svgImage.onload = function() {
-        var canvas = document.createElement("canvas");
-        canvas.width = svgImage.width * scale;
-        canvas.height = svgImage.height * scale;
+        svgImage.onload = function() {
+          var canvas = document.createElement("canvas");
+          canvas.width = svgImage.width * scale;
+          canvas.height = svgImage.height * scale;
 
-        var context = canvas.getContext("2d");
-        context.drawImage(svgImage, 0, 0, canvas.width, canvas.height);
+          var context = canvas.getContext("2d");
+          context.drawImage(svgImage, 0, 0, canvas.width, canvas.height);
 
-        pngImage.src = canvas.toDataURL("image/png");
-        pngImage.width = svgImage.width;
-        pngImage.height = svgImage.height;
+          pngImage.src = canvas.toDataURL("image/png");
+          pngImage.width = svgImage.width;
+          pngImage.height = svgImage.height;
         
-        if (callback !== undefined) {
-          callback(null, pngImage);
+          if (callback !== undefined) {
+            callback(null, pngImage);
+          }
         }
-      }
       
-      svgImage.onerror = function(e) {
-        if (callback !== undefined) {
-          callback(e);
+        svgImage.onerror = function(e) {
+          var error;
+        
+          if ('error' in e) {
+            error = e.error;
+          } else {
+            error = new Error('Error loading SVG');
+          }
+        
+          if (callback !== undefined) {
+            callback(error);
+          } else {
+            throw error;
+          }
         }
-      }
       
-      svgImage.src = "data:image/svg+xml;base64," + btoa(svgXml);
+        svgImage.src = "data:image/svg+xml;base64," + b64EncodeUnicode(svgXml);
+      }
+    } catch (e) {
+      if (callback !== undefined) {
+        callback(e);
+      } else {
+        throw e;
+      }
     }
     
     if (callback === undefined) {

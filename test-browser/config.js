445526579ba4ad0f0bdaa594efc4abb00abc26a2
Michael Daines <mdaines@fastmail.com>
2018-02-22 16:45:57 -0500

    Clean up tests.

--- test-browser/config.js
+++ test-browser/config.js
@@ -1,1 +1,2 @@
-QUnit.config.autostart = false;
+// Set autostart to true if we don't see "noautostart" in the query string.
+QUnit.config.autostart = /noautostart/.test(window.location.search) == false;
--- tests/worker.js
+++
@@ -1,8 +0,0 @@
-var config = JSON.parse(decodeURIComponent(self.location.search.slice(1)));
-
-importScripts(config.path);
-
-onmessage = function(e) {
-  var result = Viz(e.data.src, e.data.options);
-  postMessage(result);
-}

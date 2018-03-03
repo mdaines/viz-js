398e01665eaaed6a54866ff6ea16598744c84cff
Niklas Mollenhauer <nikeee@users.noreply.github.com>
2018-03-03 21:13:04 +0100

    Fix image callback

--- index.d.ts
+++ index.d.ts
@@ -6,14 +6,12 @@ declare namespace VizJs {
     interface Viz {
         (src: string, opts?: VizOptsRest): string;
         (src: string, opts?: VizImageOpts): HTMLImageElement;
-        svgXmlToPngImageElement(svgXml: string, scale: number | undefined, callback: ImageCallback): void;
+        svgXmlToPngImageElement(svgXml: string, scale: number | undefined, callback: ImageCallback<HTMLImageElement>): void;
         svgXmlToPngImageElement(svgXml: string, scale?: number): HTMLImageElement;
-        svgXmlToPngBase64(svgXml: string, scale: number | undefined, callback: ImageCallback): void;
+        svgXmlToPngBase64(svgXml: string, scale: number | undefined, callback: ImageCallback<string>): void;
     }
 
-    interface ImageCallback {
-        (error: Error | null, image: HTMLImageElement): void;
-    }
+    type ImageCallback<TData> = (error: Error | null, image: TData) => void;
     
     type Format = "svg" | "xdot" | "plain" | "ps" | "json" | "png-image-element";
     type Engine = "circo" | "dot" | "fdp" | "neato" | "osage" | "twopi";

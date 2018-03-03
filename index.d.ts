32262cc56427cc8df61b59395ead7c2da2f571fd
Niklas Mollenhauer <nikeee@users.noreply.github.com>
2018-03-03 01:21:38 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -4,33 +4,36 @@ export default Viz
 declare namespace VizJs {
 
     interface Viz {
-        (src: string, opts?: VizOpts): string | HTMLImageElement
-        svgXmlToPngImageElement(svgXml: string, scale: number | undefined, callback: ImageCallback): void
-        svgXmlToPngImageElement(svgXml: string, scale?: number): HTMLImageElement
-        svgXmlToPngBase64(svgXml: string, scale: number | undefined, callback: ImageCallback): void
+        (src: string, opts?: VizOpts): string | HTMLImageElement;
+        svgXmlToPngImageElement(svgXml: string, scale: number | undefined, callback: ImageCallback): void;
+        svgXmlToPngImageElement(svgXml: string, scale?: number): HTMLImageElement;
+        svgXmlToPngBase64(svgXml: string, scale: number | undefined, callback: ImageCallback): void;
     }
 
     interface ImageCallback {
-        (error: Error | null, image: HTMLImageElement): void
+        (error: Error | null, image: HTMLImageElement): void;
     }
+    
+    type Format = "svg" | "xdot" | "plain" | "ps" | "json" | "png-image-element";
+    type Engine = "circo" | "dot" | "fdp" | "neato" | "osage" | "twopi";
 
     interface VizOpts {
-        format?: string
-        engine?: string
-        scale?: number
-        images?: Image[]
-        totalMemory?: number,
-        files?: File[]
+        format?: Format;
+        engine?: Engine;
+        scale?: number;
+        images?: Image[];
+        totalMemory?: number;
+        files?: File[];
     }
 
     interface File {
-        path: string
-        data: string
+        path: string;
+        data: string;
     }
 
     interface Image {
-        href: string
-        height: string
-        width: string
+        href: string;
+        height: string | number;
+        width: string | number;
     }
 }

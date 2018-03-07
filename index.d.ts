cc269630acd435a0dc87f5f5780453c33665bed7
Mike Daines <mdaines@fastmail.com>
2018-03-07 08:05:19 -0500

    Rewrite TypeScript definitions. Add typing tests. (#110)

--- index.d.ts
+++ index.d.ts
@@ -1,36 +1,39 @@
-declare var Viz: VizJs.Viz
-export default Viz
+declare function Viz(src: string, opts?: Viz.Options): string;
+declare function Viz(src: string, opts: Viz.ImageFormatOptions): HTMLImageElement;
 
-declare namespace VizJs {
+declare namespace Viz {
 
-    interface Viz {
-        (src: string, opts?: VizOpts): string | HTMLImageElement
-        svgXmlToPngImageElement(svgXml: string, scale: number | undefined, callback: ImageCallback): void
-        svgXmlToPngImageElement(svgXml: string, scale?: number): HTMLImageElement
-        svgXmlToPngBase64(svgXml: string, scale: number | undefined, callback: ImageCallback): void
+    function svgXmlToPngImageElement(svgXml: string, scale?: number): HTMLImageElement
+    function svgXmlToPngImageElement(svgXml: string, scale: number | undefined, callback: ImageCallback<HTMLImageElement>): void
+    function svgXmlToPngBase64(svgXml: string, scale: number | undefined, callback: ImageCallback<string>): void
+
+    interface BaseOptions {
+        engine?: string;
+        scale?: number;
+        images?: Image[];
+        files?: File[];
+        totalMemory?: number;
+    }
+    
+    interface Options extends BaseOptions {
+        format?: "svg" | "xdot" | "plain" | "ps" | "json";
     }
 
-    interface ImageCallback {
-        (error: Error | null, image: HTMLImageElement): void
-    }
-
-    interface VizOpts {
-        format?: string
-        engine?: string
-        scale?: number
-        images?: Image[]
-        totalMemory?: number,
-        files?: File[]
-    }
-
-    interface File {
-        path: string
-        data: string
+    interface ImageFormatOptions extends BaseOptions {
+        format: "png-image-element";
     }
 
     interface Image {
-        href: string
-        height: string
-        width: string
+        href: string;
+        height: string | number;
+        width: string | number;
     }
+
+    interface File {
+        path: string;
+        data: string;
+    }
+    
+    type ImageCallback<D> = (error: Error | null, data: D | null) => void;
+
 }

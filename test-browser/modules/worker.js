84ecadeb4cd963079708047d158411ab85dbef14
Michael Daines <mdaines@fastmail.com>
2018-04-11 15:22:16 -0400

    Add a basic worker test (without the module included using a script tag)

---
+++ test-browser/modules/worker.js
@@ -0,0 +1,10 @@
+QUnit.module("worker");
+
+QUnit.test("should be able to use a worker to render", function(assert) {
+  var viz = new Viz({ worker: '../viz.module' });
+  
+  return viz.renderString("digraph { a -> b; }")
+  .then(function(result) {
+    assert.ok(result);
+  });
+});

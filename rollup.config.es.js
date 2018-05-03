89556d04138b3bdc427e835128aa9a717d8bbb57
Michael Daines <mdaines@fastmail.com>
2018-05-03 12:23:47 -0400

    Use spaces instead of tabs.

--- rollup.config.es.js
+++ rollup.config.es.js
@@ -1,8 +1,8 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-	input: 'src/index.js',
-	output: {
+  input: 'src/index.js',
+  output: {
     name: 'Viz',
     file: 'build-main/viz.es.js',
     format: 'es'

1f96316547a267c9eb88bb27eaf2db5ce9abb8b0
Michael Daines <mdaines@fastmail.com>
2023-06-12 15:14:56 -0400

    Add spaces to sample graphs

--- test/manual/utils.mjs
+++ test/manual/utils.mjs
@@ -1,15 +1,15 @@
 export function makeGraph(nodeCount, randomEdgeCount = 0) {
-  let src = "digraph {";
+  let src = "digraph { ";
 
   for (let i = 0; i < nodeCount; i++) {
-    src += `node${i};`;
+    src += `node${i}; `;
   }
 
   for (let i = 0; i < randomEdgeCount; i++) {
     const s = Math.floor(nodeCount * Math.random());
     const t = Math.floor(nodeCount * Math.random());
 
-    src += `node${s} -> node${t};`;
+    src += `node${s} -> node${t}; `;
   }
 
   src += "}";

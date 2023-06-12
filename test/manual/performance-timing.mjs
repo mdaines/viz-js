f6f5e3816abaf9fc6fbb5a6af1910b2072acdcc6
Michael Daines <mdaines@fastmail.com>
2023-06-12 15:36:35 -0400

    Add more cases to performance test. Set a time limit.

--- test/manual/performance-timing.mjs
+++ test/manual/performance-timing.mjs
@@ -2,24 +2,36 @@ import { instance } from "../../src/standalone.mjs";
 import { makeGraph } from "./utils.mjs";
 
 const matrix = [
-  { nodeCount: 100, randomEdgeCount: 0, iterations: 1000 },
-  { nodeCount: 1000, randomEdgeCount: 0, iterations: 100 },
-  { nodeCount: 5000, randomEdgeCount: 0, iterations: 10 },
-  { nodeCount: 100, randomEdgeCount: 10, iterations: 1000 },
-  { nodeCount: 1000, randomEdgeCount: 100, iterations: 100 },
-  { nodeCount: 5000, randomEdgeCount: 500, iterations: 10 }
+  { nodeCount: 100, randomEdgeCount: 0 },
+  { nodeCount: 1000, randomEdgeCount: 0 },
+  { nodeCount: 5000, randomEdgeCount: 0 },
+  { nodeCount: 100, randomEdgeCount: 50 },
+  { nodeCount: 1000, randomEdgeCount: 500 },
+  { nodeCount: 5000, randomEdgeCount: 1000 },
+  { nodeCount: 100, randomEdgeCount: 100 },
+  { nodeCount: 1000, randomEdgeCount: 1000 },
+  { nodeCount: 5000, randomEdgeCount: 5000 }
 ];
 
-for (const { nodeCount, randomEdgeCount, iterations } of matrix) {
+const timeLimit = 5000;
+
+for (const { nodeCount, randomEdgeCount } of matrix) {
   const viz = await instance();
-  const label = `${nodeCount} nodes, ${randomEdgeCount} edges, ${iterations} iterations`;
+  const label = `${nodeCount} nodes, ${randomEdgeCount} edges`;
   const src = makeGraph(nodeCount, randomEdgeCount);
 
-  console.time(label);
+  let callCount = 0;
 
-  for (let i = 0; i < iterations; i++) {
+  const startTime = performance.now();
+
+  while (performance.now() - startTime < timeLimit) {
     viz.render(src);
+    callCount++;
   }
 
-  console.timeEnd(label);
+  const stopTime = performance.now();
+  const duration = (stopTime - startTime) / 1000;
+  const speed = callCount / duration;
+
+  console.log(`${label}: ${callCount} in ${duration.toFixed(2)} s, ${speed.toFixed(2)} calls/s`);
 }

a61f8799767f6cda2bb48f3e271618c7fdb481ed
Michael Daines <mdaines@fastmail.com>
2023-06-19 15:45:27 -0400

    Render in package tests

--- test/commonjs-require/index.js
+++ test/commonjs-require/index.js
@@ -1,1 +1,3 @@
 const { instance } = require("@viz-js/viz");
+
+instance().then(viz => console.log(viz.renderString("digraph { a -> b }")));
--- test/module-import/index.js
+++ test/module-import/index.js
@@ -1,1 +1,3 @@
 import { instance } from "@viz-js/viz";
+
+instance().then(viz => console.log(viz.renderString("digraph { a -> b }")));

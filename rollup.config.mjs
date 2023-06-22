0f0e1e7b14927317408c6a9f75834fe0cc839c09
Michael Daines <mdaines@fastmail.com>
2023-06-22 16:44:24 -0400

    Add license banner

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,36 +1,60 @@
-import babel from '@rollup/plugin-babel';
-import terser from '@rollup/plugin-terser';
+import babel from "@rollup/plugin-babel";
+import terser from "@rollup/plugin-terser";
+import { readFile } from "node:fs/promises";
+
+async function getBanner() {
+  const filePath = new URL("./package.json", import.meta.url);
+  const contents = await readFile(filePath, { encoding: "utf8" });
+  const packageVersion = JSON.parse(contents).version;
+
+  return `/*!
+Viz.js ${packageVersion}
+Copyright (c) 2023 Michael Daines
+
+This distribution contains other software in object code form:
+
+Graphviz
+https://www.graphviz.org
+
+Expat
+Copyright (c) 1998-2000 Thai Open Source Software Center Ltd and Clark Cooper
+Copyright (c) 2001-2022 Expat maintainers
+https://libexpat.github.io
+*/`;
+}
 
 export default [
   {
-    input: 'src/standalone.mjs',
+    input: "src/standalone.mjs",
     output: {
-      name: 'Viz',
-      file: 'lib/viz-standalone.js',
-      format: 'umd',
+      name: "Viz",
+      file: "lib/viz-standalone.js",
+      format: "umd",
+      banner: getBanner,
       plugins: [
         terser()
       ]
     },
     plugins: [
       babel({
-        babelHelpers: 'bundled',
+        babelHelpers: "bundled",
         ignore: ["./lib/encoded.mjs"]
       })
     ]
   },
   {
-    input: 'src/standalone.mjs',
+    input: "src/standalone.mjs",
     output: {
-      file: 'lib/viz-standalone.mjs',
-      format: 'es',
+      file: "lib/viz-standalone.mjs",
+      format: "es",
+      banner: getBanner,
       plugins: [
         terser()
       ]
     },
     plugins: [
       babel({
-        babelHelpers: 'bundled',
+        babelHelpers: "bundled",
         ignore: ["./lib/encoded.mjs"]
       })
     ]

efa3f746eb047261f8179d350f2ac795d61f3999
Michael Daines <mdaines@fastmail.com>
2023-09-06 16:25:57 -0400

    Reword README, use a graph object in example

--- packages/examples/parcel/index.js
+++ packages/examples/parcel/index.js
@@ -1,9 +1,19 @@
 import { graphvizVersion } from "@viz-js/viz";
 
+function randomNode() {
+  return Math.ceil(Math.random() * 10);
+}
+
+function randomGraph() {
+  return {
+    edges: Array.from({ length: 10 }, () => ({ tail: randomNode(), head: randomNode() }))
+  };
+}
+
 document.getElementById("version").appendChild(document.createTextNode(graphvizVersion));
 
 import("@viz-js/viz")
   .then(module => module.instance())
   .then(viz => {
-    document.getElementById("output").appendChild(viz.renderSVGElement("digraph { a -> b }"));
+    document.getElementById("output").appendChild(viz.renderSVGElement(randomGraph()));
   });

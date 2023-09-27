b8357c5dd2873e261afd35d837985b1898448f64
Michael Daines <1383+mdaines@users.noreply.github.com>
2023-09-27 15:03:59 -0400

    Update viz declaration file to match API docs

--- packages/viz/types/index.d.ts
+++ packages/viz/types/index.d.ts
@@ -1,97 +1,89 @@
-export function instance(): Promise<Viz>;
+export {}
 
-export const graphvizVersion: string;
+export const graphvizVersion: string
 
-export const formats: string[];
+export const formats: string[]
 
-export const engines: string[];
+export const engines: string[]
 
-export {};
-
-export type Graph = {
-  name?: string;
-  strict?: boolean;
-  directed?: boolean;
-  graphAttributes?: Attributes;
-  nodeAttributes?: Attributes;
-  edgeAttributes?: Attributes;
-  nodes?: Node[];
-  edges?: Edge[];
-  subgraphs?: Subgraph[];
-};
-
-type Attributes = {
-  [name: string]: AttributeValue;
-};
-
-type AttributeValue = string | number | boolean | HTMLString;
-
-type HTMLString = {
-    html: string;
-};
-
-type Node = {
-    name: string;
-    attributes?: Attributes;
-};
-
-type Edge = {
-    tail: string;
-    head: string;
-    attributes?: Attributes;
-};
-
-type Subgraph = {
-    name?: string;
-    graphAttributes?: Attributes;
-    nodeAttributes?: Attributes;
-    edgeAttributes?: Attributes;
-    nodes?: Node[];
-    edges?: Edge[];
-    subgraphs?: Subgraph[];
-};
-
-export type RenderOptions = {
-    format?: string;
-    engine?: string;
-    yInvert?: boolean;
-    reduce?: boolean;
-    graphAttributes?: Attributes;
-    nodeAttributes?: Attributes;
-    edgeAttributes?: Attributes;
-};
-
-export type RenderResult = SuccessResult | FailureResult;
-
-type SuccessResult = {
-    status: "success";
-    output: string;
-    errors: RenderError[];
-};
-
-type FailureResult = {
-    status: "failure";
-    output: undefined;
-    errors: RenderError[];
-};
-
-export type RenderError = {
-    level?: "error" | "warning";
-    message: string;
-};
+export function instance(): Promise<Viz>
 
 declare class Viz {
-  get graphvizVersion(): string;
+  get graphvizVersion(): string
+  get formats(): string[]
+  get engines(): string[]
+  render(input: string | Graph, options?: RenderOptions): RenderResult
+  renderString(input: string | Graph, options?: RenderOptions): string
+  renderSVGElement(input: string | Graph, options?: RenderOptions): SVGSVGElement
+  renderJSON(input: string | Graph, options?: RenderOptions): object
+}
 
-  get formats(): string[];
+export interface RenderOptions {
+  format?: string
+  engine?: string
+  yInvert?: boolean
+  reduce?: boolean
+  graphAttributes?: Attributes
+  nodeAttributes?: Attributes
+  edgeAttributes?: Attributes
+}
 
-  get engines(): string[];
+export type RenderResult = SuccessResult | FailureResult
 
-  render(input: string | Graph, options?: RenderOptions): RenderResult;
+interface SuccessResult {
+  status: "success"
+  output: string
+  errors: RenderError[]
+}
 
-  renderString(src: any, options?: RenderOptions): string;
+interface FailureResult {
+  status: "failure"
+  output: undefined
+  errors: RenderError[]
+}
 
-  renderSVGElement(src: any, options?: RenderOptions): SVGSVGElement;
+export interface RenderError {
+  level?: "error" | "warning"
+  message: string
+}
 
-  renderJSON(src: any, options?: RenderOptions): object;
+export interface Graph {
+  name?: string
+  strict?: boolean
+  directed?: boolean
+  graphAttributes?: Attributes
+  nodeAttributes?: Attributes
+  edgeAttributes?: Attributes
+  nodes?: Node[]
+  edges?: Edge[]
+  subgraphs?: Subgraph[]
+}
+
+interface Attributes {
+  [name: string]: string | number | boolean | HTMLString
+}
+
+interface HTMLString {
+  html: string
+}
+
+interface Node {
+  name: string
+  attributes?: Attributes
+}
+
+interface Edge {
+  tail: string
+  head: string
+  attributes?: Attributes
+}
+
+interface Subgraph {
+  name?: string
+  graphAttributes?: Attributes
+  nodeAttributes?: Attributes
+  edgeAttributes?: Attributes
+  nodes?: Node[]
+  edges?: Edge[]
+  subgraphs?: Subgraph[]
 }

2b6344f1bf6f4802fc037962b54a5c2147a27895
Michael Daines <1383+mdaines@users.noreply.github.com>
2026-09-21 14:42:19 -0400

    Revert change to release info script

--- scripts/collect-release-info.js
+++ scripts/collect-release-info.js
@@ -11,7 +11,7 @@ async function readPackageInfo(directory) {
   const version = object["version"];
 
   const nameMatch = name.match(/^@viz-js\/(.+)$/);
-  const versionMatch = version.match(/^\d+\.\d+\.\d+(?:-pre\.\d+)$/);
+  const versionMatch = version.match(/^\d+\.\d+\.\d+$/);
 
   if (!nameMatch) {
     throw new Error(`name didn't match expected pattern: ${name}`);
